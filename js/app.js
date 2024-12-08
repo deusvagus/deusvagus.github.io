b58e72ad0ac9a6f8ebaffd0fbf800453a69e09bf
deusvagus <102049998+deusvagus@users.noreply.github.com>
2024-12-09 01:29:08 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const App = {
     currentPage: 1,
     itemsPerPage: 1000,
     searchType: 'all',
-    customPath: localStorage.getItem('customPath') || 'music_database_V5.1.2.json',
+    customPath: localStorage.getItem('customPath') || 'music_database_V5.2.0.json',
     selectedAlbums : [],
 
     init() {
